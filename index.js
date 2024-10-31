1df6a0d48c46136c318075e85e4367b5a9635b74
Marty <78298042+UnexplainableNo47@users.noreply.github.com>
2024-10-31 08:32:44 +0200

    c*

--- index.js
+++ index.js
@@ -20,11 +20,12 @@ form.addEventListener('submit', (e) =>{
         //check if day input has been added
     errors = getAgeCalculatorErrors(day_input.value, month_input.value, year_input.value)
     
-    if(errors.length > 0){
+    if(errors.length > 0){ 
         e.preventDefault()
     }
     else{
-        day_output.innerText = 'a.value()'
+        
+        getAge(dateString)
     }
 })
 
@@ -59,7 +60,7 @@ function getAge(day_input,month_input,year_input){
     let b = d.getMonth();
     let c = d.getFullYear();
 
-    return a,b,c;
+    console.log(a)
 }
 
 
@@ -82,3 +83,90 @@ allInputs.forEach(input => {
         }
     })
 })
+
+function getAge(dateString) {
+    var now = new Date();
+    var today = new Date(now.getYear(),now.getMonth(),now.getDate());
+  
+    var yearNow = now.getYear();
+    var monthNow = now.getMonth();
+    var dateNow = now.getDate();
+  
+    var dob = new Date(dateString.substring(6,10),
+                       dateString.substring(0,2)-1,                   
+                       dateString.substring(3,5)                  
+                       );
+  
+    var yearDob = dob.getYear();
+    var monthDob = dob.getMonth();
+    var dateDob = dob.getDate();
+    var age = {};
+    var ageString = "";
+    var yearString = "";
+    var monthString = "";
+    var dayString = "";
+  
+  
+    yearAge = yearNow - yearDob;
+  
+    if (monthNow >= monthDob)
+      var monthAge = monthNow - monthDob;
+    else {
+      yearAge--;
+      var monthAge = 12 + monthNow -monthDob;
+    }
+  
+    if (dateNow >= dateDob)
+      var dateAge = dateNow - dateDob;
+    else {
+      monthAge--;
+      var dateAge = 31 + dateNow - dateDob;
+  
+      if (monthAge < 0) {
+        monthAge = 11;
+        yearAge--;
+      }
+    }
+  
+    age = {
+        years: yearAge,
+        months: monthAge,
+        days: dateAge
+        };
+  
+    if ( age.years > 1 ) yearString = " years";
+    else yearString = " year";
+    if ( age.months> 1 ) monthString = " months";
+    else monthString = " month";
+    if ( age.days > 1 ) dayString = " days";
+    else dayString = " day";
+  
+  
+    if ( (age.years > 0) && (age.months > 0) && (age.days > 0) )
+      ageString = age.years + yearString + ", " + age.months + monthString + ", and " + age.days + dayString + " old.";
+    else if ( (age.years == 0) && (age.months == 0) && (age.days > 0) )
+      ageString = "Only " + age.days + dayString + " old!";
+    else if ( (age.years > 0) && (age.months == 0) && (age.days == 0) )
+      ageString = age.years + yearString + " old. Happy Birthday!!";
+    else if ( (age.years > 0) && (age.months > 0) && (age.days == 0) )
+      ageString = age.years + yearString + " and " + age.months + monthString + " old.";
+    else if ( (age.years == 0) && (age.months > 0) && (age.days > 0) )
+      ageString = age.months + monthString + " and " + age.days + dayString + " old.";
+    else if ( (age.years > 0) && (age.months == 0) && (age.days > 0) )
+      ageString = age.years + yearString + " and " + age.days + dayString + " old.";
+    else if ( (age.years == 0) && (age.months > 0) && (age.days == 0) )
+      ageString = age.months + monthString + " old.";
+    else ageString = "Oops! Could not calculate age!";
+  
+    return ageString;
+  }
+  
+  // A bit of jQuery to call the getAge() function and update the page...
+  $(document).ready(function() {
+    $("#submitDate").click(function(e) {
+      e.preventDefault();
+  
+      $("#age").html(getAge($("input#date").val()));
+  
+    });
+  });
